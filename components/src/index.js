2c5048493f9745ae613b116e37560ce2ebca6af0
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2021-04-13 10:31:15 +0300

    Initial commit

---
+++ components/src/index.js
@@ -0,0 +1,9 @@
+// Import the react and react dom libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return <div>Hi there!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
---
+++ jsx/src/index.js
@@ -0,0 +1,33 @@
+// Import the react and react dom libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function getButtonText() {
+    return 'Click Me Now!'
+}
+
+// Create a react component
+const App = () => {
+    const buttonText = {
+        text: 'Click me'
+    };
+    const style = {backgroundColor: 'blue', color: 'red'}
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text"/>
+            {/*<button style={style}>*/}
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {buttonText.text}
+                {/*{getButtonText()}*/}
+            </button>
+        </div>
+    );
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
