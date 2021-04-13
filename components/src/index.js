e9416862d6c442021a6839ebafe2956855d428fd
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2021-04-13 11:24:39 +0300

    Reusable components

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,16 @@
 // Import the react and react dom libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"))
