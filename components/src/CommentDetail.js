cbc3b9975749094cf3c1291f5dde59d9b0b96960
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2021-04-14 20:09:09 +0300

    component reuse

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={props.avatar} />
             </a>
             <div className="content">
-                <a href="/" className="author">Sam</a>
+                <a href="/" className="author">{props.author}</a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00 PM</span>
+                    <span className="date">Today at {props.time}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{props.text}</div>
             </div>
         </div>
     );
--- components/src/index.js
+++ components/src/index.js
@@ -2,15 +2,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail
+                author="Sam"
+                time="12:00"
+                text="Nice blog post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Dima"
+                time="15:30"
+                text="Cool post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Vasya"
+                time="23:04"
+                text="Great post!"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+ReactDOM.render(<App/>, document.querySelector("#root"))
