cefc03bfe851d629863923876f38d9485b96bc41
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2021-04-16 00:05:22 +0300

    lifecycle

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    let text = season === 'winter' ? 'Burr... it is chilli' : 'Let\'s hit the beach';
     return (
         <div>
-            Season Display
+            <h1>{text}</h1>
         </div>
     )
 };
