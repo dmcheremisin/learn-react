d159a5d9ceaab4483197acbe39b2471ab449cdf5
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2021-05-22 19:33:10 +0300

    image card dynamic height

--- 07 forms and events/src/api/unsplash.js
+++ 07 forms and events/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID woPT9Nl9p2dQLG4ZYNlniW-h8ZiNE45qpt8-kZtMAKA',
+        Authorization: 'Client-ID woPT9Nl9p3dQLG4ZYNlniW-h8ZiNE45qpt8-kZtMAKA',
     }
 })
